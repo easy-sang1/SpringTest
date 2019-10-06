1f4e7d687cc602d5433857d44765cde99a8acaa5
user <user@DESKTOP-7EQF3M8>
2019-10-06 12:00:11 +0900

    첫번쨰 커밋입니다 스프링 웹사이트입니까? ver 1.0 beta

---
+++ Basic/src/main/java/com/spring/basic/commons/util/OAuthValue.java
@@ -0,0 +1,8 @@
+package com.spring.basic.commons.util;
+
+public interface OAuthValue {
+	
+	String KAKAO_APP_KEY = "ecf1c6ee28d83ab475d5795c3bd1809c";
+	String KAKAO_REDIRECT_PATH = "/auth/kakao";
+	
+}
